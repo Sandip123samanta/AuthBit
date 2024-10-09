7d7b73a1d34052ef094e565c0adcfcbb62135f02
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2024-10-10 00:59:27 +0530

    signup and email verification page created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { connectDB } from './db/connectDB.js';
 import authRoute from './routes/auth.route.js';
@@ -9,6 +10,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
